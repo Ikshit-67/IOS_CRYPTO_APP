134e8b97816ef062864b0803ea55cfd84414e035
Ikshit C <109130289+Ikshit-67@users.noreply.github.com>
2023-01-05 00:12:18 +0530

    first commit

---
+++ crypto_app/Core/Home/Views/TopMoversItemView.swift
@@ -0,0 +1,20 @@
+//
+//  AllCoinsView.swift
+//  crypto_app
+//
+//  Created by Ikshit Chaudhari on 04/01/23.
+//
+
+import SwiftUI
+
+struct AllCoinsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AllCoinsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AllCoinsView()
+    }
+}
